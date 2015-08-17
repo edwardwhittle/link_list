7180b3359bee5c1015b6a7cbdaa266e5ecdedddc
Edward Whittle <edward.whittle1@live.vu.edu.au>
2015-08-17 20:42:27 +1000

    Second Commit

--- hello_world.c
+++ hello_world.c
@@ -6,18 +6,22 @@ struct s_word_object{
 	char *word;
 	word_object *next;
 };
-
+static word_object *list_head;
 
 //Add object to list
-
+static void add_to_list(char *word) {
+}
 
 //Print and free objects
 
 
 
 int main(void) {
+char input_word[256];
 
-while(1){
+
+while(scanf("%256s", input_word) != EOF){
+	printf("%s\n",input_word);
 //Collect input strings
 
 
