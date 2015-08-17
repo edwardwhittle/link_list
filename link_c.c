f6cdf7bb9891308bb92d2d2d07fd2f011dfa4450
Edward Whittle <edward.whittle1@live.vu.edu.au>
2015-08-17 20:22:25 +1000

    Link List v1

---
+++ link_c.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// Linked list object
+typedef struct s_word_object word_object; //make word_object = struct s_word_object
+struct s_word_object{
+	char *word;
+	word_object *next;
+};
+
+
+//Add object to list
+
+
+//Print and free objects
+
+
+
+int main(void) {
+
+while(1){
+//Collect input strings
+
+
+//As to list
+//Check count
+}
+//Print and free objects
+
+
+
+
+	return 0;
+	}
+
